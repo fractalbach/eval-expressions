313350accab3b2951ec139e55f76298e44fde471
fractalbach <fractalbach@gmail.com>
2019-03-03 15:36:50 -0800

    added lexer in seperate package

--- main.go
+++ main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
-	"unicode"
 
 	"github.com/fatih/color"
+	"github.com/fractalbach/eval-expressions/lexer"
 )
 
+var (
+	lex             = lexer.New()
+	interactiveFlag = false
+)
+
+func init () {
+	flag.BoolVar(&interactiveFlag, "i", false, "use interactive mode.")
+}
+
 func main() {
-	interactiveMode()
+	flag.Parse()
+	if interactiveFlag {
+		interactiveMode()
+		return
+	}
+	plainMode()
 }
 
 func interactiveMode() {
@@ -23,6 +38,16 @@ func interactiveMode() {
 	}
 	if s.Err() != nil {
 		showErr(s.Err())
+	}
+}
+
+func plainMode() {
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		plainEval(s.Text())
+	}
+	if s.Err() != nil {
+		fmt.Fprintln(os.Stdout, s.Err())
 	}
 }
 
@@ -45,110 +70,22 @@ func showErr(err interface{}) {
 }
 
 func eval(s string) {
-	tokenize(s)
+	lex.Tokenize(s)
+	err := lex.Error()
+	if err != nil {
+		showErr(err)
+	} else {
+		color.Set(color.FgGreen)
+		lex.Display()
+		color.Unset()
+	}
 }
 
-type token struct {
-	kind    string
-	content string
-}
-
-var (
-	content     = ""
-	kind        = ""
-	insideToken = false
-	dotUsedAlready = false
-)
-
-func tokenize(s string) {
-	resetTokenizer()
-	for _, r := range s {
-		switch r {
-		case '(', ')', '+', '-', '*', '/', '=':
-			clearToken()
-			xml("symbol", r)
-			continue
-			
-		case '.':
-			if kind == "" {
-				tokErr("cannot use \".\" to start a number")
-				return
-			}
-			if  kind != "number" {
-				tokErr("cannot use \".\" outside number")
-				return
-			}
-			if dotUsedAlready {
-				tokErr("too many \".\"s in a number")
-				return
-			} 
-			dotUsedAlready = true
-			content += "."
-			continue
-		}
-		switch {
-		case unicode.IsSpace(r):
-			// clearToken()
-			continue
-
-		case unicode.IsDigit(r):
-			if !insideToken {
-				startToken("number")
-			}
-			content += string(r)
-			continue
-
-		case unicode.IsLetter(r):
-			if !insideToken {
-				startToken("identifier")
-			}
-			content += string(r)
-			continue
-		}
-		tokErr(`Invalid symbol "` + string(r) + `"`)
+func plainEval(s string) {
+	lex.Tokenize(s)
+	if lex.Error() != nil {
+		fmt.Fprintln(os.Stdout, lex.Error())
 		return
 	}
-	clearToken()
-}
-
-func tokErr(i ...interface{}) {
-	showErr("Error: That's a mean expression you have.")
-	errMessage := fmt.Sprint(i...)
-	showErr(errMessage)
-}
-
-func startToken(name string) {
-	insideToken = true
-	kind = name
-}
-
-func clearToken() {
-	if insideToken {
-		xml(kind, content)
-	}
-	resetTokenizer()
-}
-
-func resetTokenizer() {
-	insideToken = false
-	kind = ""
-	content = ""
-	dotUsedAlready = false
-}
-
-func spaces(depth int) {
-	s := ""
-	for i := 0; i < depth; i++ {
-		s += " "
-	}
-	fmt.Print(s)
-}
-
-func xml(name string, i interface{}) {
-	r, ok := i.(rune)
-	if ok {
-		fmt.Printf("<%s> %c </%s>\n", name, r, name)
-	} else {
-		fmt.Printf("<%s> %s </%s>\n", name, i, name)
-	}
+	lex.Display()
 }
